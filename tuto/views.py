96bfbe9945528ff4bdf770a20824807d25b7390e
Cochet Raphael <o22301249@or-iut-i203-f32.iut45.univ-orleans.fr>
2024-09-17 12:05:21 +0200

    TP2 fin

--- tuto/views.py
+++ tuto/views.py
@@ -1,7 +1,20 @@
 from .app import app
+from flask import render_template
+from .models import get_sample
 
 
 @app.route("/")
 def home():
-    return "<h1>Hello World !</h1>"
+    return render_template(
+        "booksBS.html", 
+        title="My Books !",
+        books=get_sample())
 
+
+@app.route("/detail/<id>")
+def detail(id):
+    books = get_sample()
+    book = books[int(id)]
+    return render_template(
+        "detail.html",
+        book=book)
