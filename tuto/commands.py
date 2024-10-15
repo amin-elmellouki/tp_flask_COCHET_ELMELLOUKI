d9e61b32b197b1d6d49633ed70cc94c320809953
Amin ELMELLOUKI <amin.el-mellouki@etu.univ-orleans.fr>
2024-10-15 12:29:24 +0200

    ajout de la commande newUser

--- tuto/commands.py
+++ tuto/commands.py
@@ -41,3 +41,16 @@ def loaddb(filename):
 def syncdb():
     '''Creates all missing tables.'''
     db.create_all()
+
+@app.cli. command ()
+@click.argument('username')
+@click.argument('password')
+def newuser(username, password):
+    '''Adds a new user.'''
+    from .models import User
+    from hashlib import sha256
+    m = sha256()
+    m.update(password.encode())
+    u = User(username=username, password=m.hexdigest())
+    db.session.add(u)
+    db.session.commit()
