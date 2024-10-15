57f7c525109c1cfa812fe12b53fcef65c0f44726
Cochet Raphael <o22301249@or-iut-i203-f05.iut45.univ-orleans.fr>
2024-10-15 12:44:28 +0200

    réparation loginmanager

--- tuto/app.py
+++ tuto/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
-from flask import LoginManager
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
