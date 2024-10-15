b3602c8c58102869ae72d8adfdabd171bb66ae33
Cochet Raphael <o22301249@or-iut-i203-f05.iut45.univ-orleans.fr>
2024-10-15 12:19:44 +0200

    Ajout de la fonctionnalitée ajout d'un author avec /add/author

--- tuto/views.py
+++ tuto/views.py
@@ -1,63 +1,79 @@
-from .app import app
-from .models import get_author, get_sample
+from flask import url_for, redirect, render_template, flash
+from .app import app, db
+from .models import Author, get_author, get_sample
 from flask_wtf import FlaskForm
 from wtforms import StringField, HiddenField
 from wtforms.validators import DataRequired
-from flask import url_for, redirect, render_template
-from .app import db
-from .models import Author
-from .models import get_author
+
+
+class AuthorForm(FlaskForm):
+    id = HiddenField('id') 
+    name = StringField('Nom', validators=[DataRequired()])
 
 @app.route("/")
 def home():
     return render_template(
-        "booksBS.html", 
+        "booksBS.html",
         title="My Books !",
-        books=get_sample())
-
+        books=get_sample()
+    )
 
 @app.route("/detail/<id>")
 def detail(id):
     books = get_sample()
     book = books[int(id)-1]
-    return render_template(
-        "detail.html",
-        book=book)
+    return render_template("detail.html", book=book)
 
-class AuthorForm(FlaskForm):
-    id = HiddenField('id')
-    name = StringField('Nom', validators=[DataRequired()])
+@app.route("/add/author", methods=["GET", "POST"])
+def add_author():
+    form = AuthorForm()
+
+    if form.validate_on_submit():
+        new_author = Author(name=form.name.data)
+        db.session.add(new_author)
+        db.session.commit()
+
+        flash(f"Auteur '{new_author.name}' ajouté avec succès!", 'success')
+        return redirect(url_for('one_author', id=new_author.id))
+
+    return render_template("add-author.html", form=form)
 
 @app.route("/edit/author/<int:id>")
 def edit_author(id):
     a = get_author(id)
-    f = AuthorForm(id=a.id, name=a.name)
+    if not a:
+        return "Author not found", 404
+
+    form = AuthorForm(id=a.id, name=a.name)
     return render_template(
         "edit-author.html",
-        author=a, form=f
+        author=a, form=form
     )
 
-@app.route("/save/author/", methods=("POST",))
+@app.route("/save/author", methods=["POST"])
 def save_author():
-    a = None
-    f = AuthorForm()
+    form = AuthorForm()
 
-    if f.validate_on_submit():
-        id = int(f.id.data)
-        a = get_author(id)
-        a.name = f.name.data
+    if form.validate_on_submit():
+        if form.id.data:
+            author = get_author(int(form.id.data))
+            author.name = form.name.data
+        else:
+            author = Author(name=form.name.data)
+            db.session.add(author)
+
         db.session.commit()
-        return redirect(url_for('one_author', id=a.id))
+        flash(f"Auteur '{author.name}' enregistré avec succès!", 'success')
+        return redirect(url_for('one_author', id=author.id))
 
-    a = get_author(int(f.id.data))
-    return render_template("edit-author.html", author=a, form=f)
+    return render_template("add-author.html", form=form)
 
-@app.route("/one_author/<int:id>", methods=["GET"])
+@app.route("/one_author/<int:id>")
 def one_author(id):
     a = get_author(id)
     if not a:
         return "Author not found", 404
     return render_template(
-        "detail_author.html", 
+        "detail_author.html",
         author=a
     )
